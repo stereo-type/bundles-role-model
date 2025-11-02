c28d8268950c53af25cf68500fe683a7d49d5026
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2025-11-02 16:47:08 +0300

    2.0.12 CapabilityAction extend

--- src/Application/Enum/CapabilityAction.php
+++ src/Application/Enum/CapabilityAction.php
@@ -12,9 +12,13 @@ namespace Slcorp\RoleModelBundle\Application\Enum;
 enum CapabilityAction: string
 {
     case CREATE = 'create';
+    case CREATE_SELF = 'create_self';
     case EDIT = 'edit';
+    case EDIT_SELF = 'edit_self';
     case DELETE = 'delete';
+    case DELETE_SELF = 'delete_self';
     case VIEW = 'view';
+    case VIEW_SELF = 'view_self';
     case LIST = 'list';
 
     public function name(): string
@@ -25,6 +29,10 @@ enum CapabilityAction: string
             self::DELETE => 'Удаление',
             self::VIEW => 'Просмотр',
             self::LIST => 'Просмотр списка',
+            self::VIEW_SELF => 'Просмотр своего',
+            self::CREATE_SELF => 'Создание свое',
+            self::EDIT_SELF => 'Редактирование свое',
+            self::DELETE_SELF => 'Удаление свое',
         };
     }
 }
